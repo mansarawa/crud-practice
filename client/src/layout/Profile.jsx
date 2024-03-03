bd05c202cb3943d4d20d998e75f383d38b49beeb
mansarawa <you@example.com>
2024-03-03 19:15:22 +0530

    user fetch success

--- client/src/layout/Profile.jsx
+++ client/src/layout/Profile.jsx
@@ -1,60 +1,63 @@
-import React,{useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
+import proFile from './profile.module.css'
 import { toast } from 'react-toastify';
-
-export default  function Profile() {
+import ClickAwayListener from 'react-click-away-listener';
+import { useNavigate } from 'react-router-dom';
+export default function Profile() {
+  const [photomodal, setPhotomodal] = useState(false)
   const [photo, setPhoto] = useState('')
-  const user=JSON.parse(localStorage.getItem('user'))
-  const userid=user._id;
+  const user = JSON.parse(localStorage.getItem('user'))
+  const userid = user._id;
+  const navigate=useNavigate()
   const [image, setImage] = useState()
-  const handleupload=async()=>{
-    const data=new FormData();
-    data.append('file',image)
-    data.append("upload_preset","mansanotebook")
-    data.append("cloud_name","do8hfvnjb")
-    const cloudSend=await fetch('https://api.cloudinary.com/v1_1/do8hfvnjb/image/upload',{
-      method:'post',
-      body:data
+  const handleupload = async () => {
+    const data = new FormData();
+    data.append('file', image)
+    data.append("upload_preset", "mansanotebook")
+    data.append("cloud_name", "do8hfvnjb")
+    const cloudSend = await fetch('https://api.cloudinary.com/v1_1/do8hfvnjb/image/upload', {
+      method: 'post',
+      body: data
 
     })
-    const resu=await cloudSend.json();
+    const resu = await cloudSend.json();
     console.log(resu)
-    const imageUrl=resu.url;      
-      console.log(imageUrl)
+    const imageUrl = resu.url;
+    console.log(imageUrl)
 
-    const req=await fetch('http://localhost:4000/profile',{
-      method:'post',
-      headers:{
-        'Content-Type':'application/json'
+    const req = await fetch('http://localhost:4000/profile', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json'
       },
-      body:JSON.stringify({
-        url:imageUrl,
-        userid:userid
+      body: JSON.stringify({
+        url: imageUrl,
+        userid: userid
       })
     })
-    const result=await req.json();
-    if(result.success)
-    {
+    const result = await req.json();
+    if (result.success) {
       console.log('uploaded')
+      navigate('/profile')
       toast.success("image uploaded")
     }
   }
-  const getUserProfilePic = async() =>{
+  const getUserProfilePic = async () => {
 
-    const profile=await fetch('http://localhost:4000/getprofile',{
-      method:'post',
-      headers:{
-        'Content-Type':'application/json'
+    const profile = await fetch('http://localhost:4000/getprofile', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json'
       },
-      body:JSON.stringify({
-        userid:userid
+      body: JSON.stringify({
+        userid: userid
       })
     })
     console.log('hello')
-    const output=await profile.json();
+    const output = await profile.json();
 
-   console.log(output.data)
-    if(output.success)
-    {
+    console.log(output.data)
+    if (output.success) {
       setPhoto(output.data.url)
       console.log(photo)
     }
@@ -62,13 +65,38 @@ export default  function Profile() {
   useEffect(() => {
     getUserProfilePic();
   }, [])
+  const handleedit = async () => {
+    setPhotomodal(true)
+  }
+  const handleClickAway = () => {
+    setPhotomodal(false)
+  }
   return (
-    <div className='container'>
-       <div className="image" style={{height:"200px", width:"200px",overflow:"hidden"}}>
-        <img src={photo} alt="" style={{height:"200px", width:"200px",objectFit:"contain"}}/>
-      </div> 
-      <input type="file" onChange={(e)=>setImage(e.target.files[0])}/>
-      <button onClick={handleupload}>Upload</button>
+    <div className={`container my-3 ${proFile.container}`} style={{textAlign:'center'}}>
+      <div className="image" style={{ overflow: "hidden" }}>
+        <img src={photo} alt="" style={{ height: "150px", borderRadius: '70%', width: "150px", objectFit:"fill" }} />
+        <button className={`btn btn-primary  ${proFile.edit}`} onClick={handleedit}>Edit Profile Photo</button>
+      </div>
+      {photomodal && (
+        <ClickAwayListener onClickAway={handleClickAway}>
+          <div>
+            <input type="file"  onChange={(e) => setImage(e.target.files[0])} />
+            <button className ="btn btn-primary" onClick={handleupload}>Upload</button>
+          </div>  
+        </ClickAwayListener>)}
+        <div className={proFile.userdetails}>
+          <div className={proFile.item}>
+            <h3 style={{width:'50%'}}>Name </h3>
+            <h4 style={{width:'50%'}}>{user.name}</h4>
+          </div>
+          <div className={proFile.item}>
+          <h3 style={{width:'50%'}}>Email </h3> <h4 style={{width:'50%'}}>{user.email}</h4>
+          </div>
+          <div className={proFile.item}>
+          <h3 style={{width:'50%'}}>Password </h3> <h4 style={{width:'50%'}}>{user.password}</h4>
+          </div>
+
+        </div>
     </div>
   )
 }
